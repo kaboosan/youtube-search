744d384f6ed7da85d93e91910a4d19008fab34d3
Kamel Bourahla <bourahla.kamel@gmail.com>
2019-05-10 06:11:49 +0200

    update

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyBc8gpLYSdOk7zss7MY0KucH2Q5uWBB2l8';
+const KEY = '';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
